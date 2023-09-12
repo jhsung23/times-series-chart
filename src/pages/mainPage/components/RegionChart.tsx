8b66c6a0d01df6eb2763b7ff397aec4e9b2de183
sungjihyun <jhsung23@gmail.com>
2023-09-12 13:21:02 +0900

    feat: 특정 데이터 구역으로 필터링

--- src/pages/mainPage/components/RegionChart.tsx
+++ src/pages/mainPage/components/RegionChart.tsx
@@ -1,24 +1,41 @@
-import { Area, Bar, ComposedChart, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+import {
+  Area,
+  Bar,
+  Cell,
+  ComposedChart,
+  Legend,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 import { Heading } from '@/components';
+import { ChipSet } from '@/components/Chip';
 import { mockData } from '@/data/mockData';
+import { useChip } from '@/hooks';
+import { getUniqueAttributes, getValuesFromObjectArray, makeKeyToAttributeOfValue } from '@/utils';
 
 const CHART_NAME = `Time-series Chart of ${Object.keys(mockData.response)[0].split(' ')[0]}`;
 
-const data = Object.keys(mockData.response).map((key) => ({
-  time: key.split(' ')[1],
-  ...mockData.response[`${key}`],
-}));
+const chartDatas = makeKeyToAttributeOfValue(mockData.response, 'time');
+const filterChipValues = getValuesFromObjectArray(getUniqueAttributes(chartDatas, 'id'));
 
 const RegionChart = () => {
+  const { selectedChip, changeSelectedChip } = useChip();
+  const filteredDatas = new Set(
+    chartDatas.filter((data) => data.id === selectedChip).map((data) => data.time),
+  );
+
   return (
     <>
       <Heading size="medium">{CHART_NAME}</Heading>
+      <ChipSet
+        values={filterChipValues}
+        selectedChip={selectedChip}
+        changeSelectedChip={changeSelectedChip}
+      />
       <ResponsiveContainer width={'85%'} height={600}>
-        <ComposedChart
-          data={Object.values(data)}
-          margin={{ top: 0, right: 24, bottom: 0, left: 24 }}
-        >
+        <ComposedChart data={chartDatas} margin={{ top: 0, right: 24, bottom: 0, left: 24 }}>
           <XAxis dataKey="time" domain={['dataMin', 'dataMax']} />
           <YAxis
             label={{ value: 'value_bar', angle: -90, position: 'left' }}
@@ -33,8 +50,12 @@ const RegionChart = () => {
             domain={[0, 200]}
           />
           <Legend />
-          <Bar yAxisId={'left'} dataKey="value_bar" fill="#BBDED6" />
-          <Area yAxisId={'right'} dataKey="value_area" fill="#fab1b3" stroke="#ff8589" />
+          <Bar yAxisId={'left'} dataKey="value_bar" fill="#b0dbd0">
+            {chartDatas.map((data) => (
+              <Cell key={data.time} fill={filteredDatas.has(data.time) ? '#2dbebb' : '#b0dbd0'} />
+            ))}
+          </Bar>
+          <Area yAxisId={'right'} dataKey="value_area" fill="#fd9a9d" stroke="#ff8589" />
         </ComposedChart>
       </ResponsiveContainer>
     </>
